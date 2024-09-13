806f27d306f0501bb3cc532452b313c97e3373cb
Frouen M. Medina Jr. <117834496+WenDEVLIFE@users.noreply.github.com>
2024-09-13 08:56:37 +0800

    added commits

---
+++ lib/FirebaseRun.dart
@@ -0,0 +1,39 @@
+
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+
+class firebaserun {
+  static Future<void> run() async {
+    await Firebase.initializeApp(
+        options: const FirebaseOptions(
+        apiKey: 'AIzaSyBVhVZIGWKvPagJgX0QW7to_U7FcjqVpXo',
+        appId: '914861841567',
+        messagingSenderId: '914861841567',
+        projectId: 'qr-attendance-database',
+    ),
+    );
+
+    if (Firebase.apps.isEmpty) {
+      Fluttertoast.showToast(
+          msg: "Firebase initialization failed",
+          toastLength: Toast.LENGTH_SHORT,
+          gravity: ToastGravity.BOTTOM,
+          timeInSecForIosWeb: 1,
+          backgroundColor: Colors.black,
+          textColor: Colors.white,
+          fontSize: 16.0
+      );
+    } else {
+      Fluttertoast.showToast(
+          msg: "Firebase is already initialized",
+          toastLength: Toast.LENGTH_SHORT,
+          gravity: ToastGravity.BOTTOM,
+          timeInSecForIosWeb: 1,
+          backgroundColor: Colors.black,
+          textColor: Colors.white,
+          fontSize: 16.0
+      );
+    }
+  }
+}
