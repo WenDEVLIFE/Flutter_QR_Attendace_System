8b041de12cf29bd896b84171fd13080757689756
Frouen M. Medina Jr. <117834496+WenDEVLIFE@users.noreply.github.com>
2024-10-18 11:00:07 +0800

    Added EvenntAttendanceScreen

---
+++ lib/Screens/EventAttendanceScreen.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/cupertino.dart';
+
+class EventAttendanceScreen extends StatefulWidget {
+  const EventAttendanceScreen({super.key});
+
+  @override
+  EventAttendanceState createState() => EventAttendanceState();
+}
+
+class EventAttendanceState extends State<EventAttendanceScreen>{
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+}
