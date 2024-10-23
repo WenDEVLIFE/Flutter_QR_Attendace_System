884939f77b48199a35b4f12a2585693e212dcff5
Wenwen67 <medinajrfrouen@gmail.com>
2024-10-23 10:40:10 +0800

    Added StudentModel.dart 👾

---
+++ lib/model/StudentModel.dart
@@ -0,0 +1,36 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class StudentModel {
+  String id;
+
+  int studentID;
+
+  String FullName;
+
+  String Grade;
+
+  String Section;
+
+  String Gender;
+
+
+  StudentModel({
+    required this.id,
+    required this.studentID,
+    required this.FullName,
+    required this.Grade,
+    required this.Section,
+    required this.Gender,
+  });
+
+  factory StudentModel.fromDocument(DocumentSnapshot doc) {
+    return StudentModel(
+      id: doc.id,
+      studentID: doc['StudentID'],
+      FullName: doc['FullName'],
+      Grade: doc['Grade'],
+      Section: doc['Section'],
+      Gender: doc['Gender']
+    );
+  }
+}
