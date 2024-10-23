cc7837aa42a373b49717c9e06ceb489f6d136c59
Wenwen67 <medinajrfrouen@gmail.com>
2024-10-23 16:35:26 +0800

    Added student screen to the main controller and updates 👾

--- lib/Component/MainController.dart
+++ lib/Component/MainController.dart
@@ -167,7 +167,7 @@ class _MaincontrollerState extends State<Maincontroller> {
                   ShowProfile(context).showProfile(username: username, fullname: fullname, role: role, imageURL: ImageUrl, Load: Load);
                   return;
                 }
-                if (index == 6) {
+                if (index == 5) {
                   Logout();
                   return;
                 } else {
